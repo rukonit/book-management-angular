696028b35c4ab4683e98e5f8149d5c866edc1a29
rukonit <rukon-it@live.com>
2021-08-06 00:46:57 -0400

    added environment variable

--- src/app/books/store/book.effects.ts
+++ src/app/books/store/book.effects.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { Actions, Effect, ofType } from "@ngrx/effects";
 import { Store } from "@ngrx/store";
 import { map, switchMap } from "rxjs/operators";
+import { environment } from "src/environments/environment";
 import * as fromAppState from '../../store/app.reducer';
 import { Book } from "../book.model";
 import * as fromBookActions from '../store/book.actions';
@@ -16,7 +17,7 @@ export class BookEffects {
     @Effect()
     fetchBooks = this.action$.pipe(ofType(fromBookActions.FETCH_BOOKS),
     switchMap(()=>{
-        return this.http.get<Book[]>("http://localhost:8081/rest/books")}),
+        return this.http.get<Book[]>(environment.baseUrl + "/rest/books")}),
         map(books => {
            
             
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: "https://morning-meadow-05272.herokuapp.com"
 };
