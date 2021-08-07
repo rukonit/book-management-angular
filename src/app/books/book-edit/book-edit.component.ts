382f55e3d935d46f6ba94ce89905f45d3e503565
rukonit <rukon-it@live.com>
2021-08-07 13:45:56 -0400

    redesigned form component

--- src/app/books/book-edit/book-edit.component.ts
+++ src/app/books/book-edit/book-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { map, switchMap } from 'rxjs/operators';
 import { Book } from '../book.model';
